478fe340776c23eaa78b86008a68f17f588c660c
Ajay-Dhakad <ajaydhakad.py@gmail.com>
2024-01-12 17:38:26 +0530

    updated and  removed unused code !

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 
 function App() {
